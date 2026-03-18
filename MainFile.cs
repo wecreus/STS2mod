999d181b86c71c2d81db50016428421f3dd5f523
Danylo <danylo@Danylos-MacBook-Pro.local>
2026-03-18 04:52:05 +0200

    initial commit

---
+++ MainFile.cs
@@ -0,0 +1,21 @@
+using Godot;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Modding;
+
+namespace TestMod2Lol;
+
+[ModInitializer(nameof(Initialize))]
+public partial class MainFile : Node
+{
+    public const string ModId = "TestMod2Lol"; //At the moment, this is used only for the Logger and harmony names.
+
+    public static MegaCrit.Sts2.Core.Logging.Logger Logger { get; } =
+        new(ModId, MegaCrit.Sts2.Core.Logging.LogType.Generic);
+
+    public static void Initialize()
+    {
+        Harmony harmony = new(ModId);
+
+        harmony.PatchAll();
+    }
+}
